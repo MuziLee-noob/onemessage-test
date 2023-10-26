37bee4bd13b7151df2020fe7cd8126680752c57f
lijinhe <lijinhe@cmict.chinamobile.com>
2023-10-26 17:38:36 +0800

    update: 依赖和配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  productionSourceMap: false,
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.resolve(__dirname, "./src/styles/variable.less")]
+    }
+  },
+  transpileDependencies: ["htmlparser2", "parse5"]
+}
